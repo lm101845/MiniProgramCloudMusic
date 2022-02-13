035ef3b282101eb90b9eeec8e77f603d244f6f68
lm101845 <1018457683@qq.com>
2022-02-13 22:52:10 +0800

    看到第74集

--- 02-代码手敲/cloudMusic/pages/recommendSong/recommendSong.js
+++ 02-代码手敲/cloudMusic/pages/recommendSong/recommendSong.js
@@ -1,3 +1,5 @@
+import PubSub, { publish } from 'pubsub-js'; //我们是订阅方
+
 import request from '../../utils/request.js';
 
 
@@ -11,6 +13,7 @@ Page({
         month: '', //月
         //在小程序里面其实也是有内置的日期函数的
         recommendList: [], //推荐列表数据
+        index: 0, //当前播放的歌曲的索引
     },
 
     /**
@@ -39,6 +42,26 @@ Page({
 
         //获取每日推荐的数据
         this.getRecommendList();
+
+        //订阅来自songDetail页面发布的消息
+        PubSub.subscribe('switchType', (msg, type) => {
+            console.log(msg, type);
+            let { recommendList, index } = this.data;
+            if (type === 'pre') { //上一首
+                (index === 0 ) && (index = recommendList.length);
+                    index -= 1
+            } else { //下一首
+                (index === recommendList.length - 1) && (index = -1);
+                index += 1
+            }
+            //更新下标
+            this.setData({
+                index
+            })
+            let musicId = recommendList[index].id;
+            //将musicId回传给songDetail页面
+            publish('musicId', musicId);
+        })
     },
 
     //获取用户每日推荐的数据
@@ -50,9 +73,14 @@ Page({
     },
     // 跳转至songDetail页面
     toSongDetail(event) {
-        let song = event.currentTarget.dataset.song;
-        // console.log(song,'123121321');
-        // 如何在路由跳转时传参:query参数
+        // let song = event.currentTarget.dataset.song;
+        // let index = event.currentTarget.dataset.index;
+        let { song, index } = event.currentTarget.dataset;
+        this.setData({
+                index
+            })
+            // console.log(song,'123121321');
+            // 如何在路由跳转时传参:query参数
         wx.navigateTo({
             // url: '/pages/songDetail/songDetail?song=' + song,
             //在js里面是不能有对象形式的数组的
--- 02-代码手敲/cloudMusic/pages/songDetail/songDetail.js
+++ 02-代码手敲/cloudMusic/pages/songDetail/songDetail.js
@@ -1,4 +1,8 @@
+import PubSub from 'pubsub-js'; //我们是发布方
+import moment from 'moment';
 import request from '../../utils/request.js';
+//获取全局实例
+const appInstance = getApp();
 
 Page({
     /**
@@ -9,6 +13,10 @@ Page({
         song: {}, //歌曲信息
         musicId: "", //歌曲id
         // backgroundAudioManager:{} //音乐播放器
+        musicLink: "", //音乐链接
+        currentTime: "0.00", //当前播放时间
+        durationTime: "00:00", //音乐总时长
+        currentWidth: 0, //当前进度条宽度
     },
 
     /**
@@ -28,13 +36,21 @@ Page({
             // console.log(musicId, 'musicId');
             // console.log(options, 'options');
             //获取音乐详情
-        this.getUserInfo(musicId)
+        this.getMusicInfo(musicId)
 
         /***
          * 如果用户操纵系统的控制音乐播放/暂停的按钮，页面不知道，导致页面显示是否播放的状态和真实的音乐播放状态不一致
          * 解决方案：
          *  1.通过控制音频的实例去监视音乐播放/暂停
          */
+
+        //判断当前页面音乐是否在播放
+        if (appInstance.globalData.isMusicPlay && appInstance.globalData.musicId === musicId) {
+            //修改当前页面的音乐播放状态为true
+            this.setData({
+                isPlay: true
+            })
+        }
         //创建控制音乐播放的实例
         this.backgroundAudioManager = wx.getBackgroundAudioManager();
         //监视音乐播放/暂停
@@ -44,6 +60,10 @@ Page({
             //     isPlay: true
             // })
             this.changePlayStatus(true)
+                //修改全局播放的状态
+                // appInstance.globalData.isMusicPlay = true;
+            appInstance.globalData.musicId = musicId;
+
         })
         this.backgroundAudioManager.onPause(() => {
             // console.log('pause');
@@ -51,26 +71,56 @@ Page({
             //     isPlay: false
             // })
             this.changePlayStatus(false)
+                //修改全局播放的状态
+                // appInstance.globalData.isMusicPlay = false;
         })
         this.backgroundAudioManager.onStop(() => {
             // this.setData({
             //     isPlay: false
             // })
             this.changePlayStatus(false)
+                //修改全局播放的状态
+                // appInstance.globalData.isMusicPlay = false;
         })
+
+        //监听音乐播放进度
+        this.backgroundAudioManager.onTimeUpdate(() => {
+            // console.log('总时长', this.backgroundAudioManager.duration);
+            // console.log('实时时间', this.backgroundAudioManager.currentTime);
+            //格式化实时的播放时间
+            let currentTime = moment(this.backgroundAudioManager.currentTime * 1000).format('mm:ss');
+            let currentWidth = (this.backgroundAudioManager.currentTime / this.backgroundAudioManager.duration) * 450;
+            this.setData({
+                currentTime,
+                currentWidth
+            })
+        })
+
+        // //订阅来自recommendSong页面发布的musicId消息
+        // PubSub.subscribe("musicId", (msg, musicId) => {
+        //     console.log(msg, musicId);
+        //     //取消订阅(订阅完拿到数据后就取消订阅，因为已经没有利用价值了)
+        //     PubSub.unsubscribe("musicId");
+        // })
+
+
     },
 
     //修改播放状态的功能函数
     changePlayStatus(isPlay) {
         this.setData({
-            isPlay
-        })
+                isPlay
+            })
+            //修改全局播放的状态
+        appInstance.globalData.isMusicPlay = isPlay;
     },
     //获取音乐详情的功能函数
-    async getUserInfo(musicId) {
+    async getMusicInfo(musicId) {
         let songData = await request('/song/detail', { ids: musicId });
+        let durationTime = moment(songData.songs[0].dt).format('mm:ss')
         this.setData({
-                song: songData.songs[0]
+                song: songData.songs[0],
+                durationTime
             })
             //动态修改窗口的标题
         wx.setNavigationBarTitle({
@@ -79,7 +129,7 @@ Page({
     },
 
     handleMusicPlay() {
-        let { musicId } = this.data
+        let { musicId, musicLink } = this.data
             // 点击播放或暂停的回调
         let isPlay = !this.data.isPlay;
         //修改是否播放的状态——现在已经不需要了
@@ -87,16 +137,21 @@ Page({
         //         isPlay
         //     })
         //除了要修改状态以外，还要告诉功能函数你是要播放还是暂停
-        this.musicControl(isPlay, musicId);
+        this.musicControl(isPlay, musicId, musicLink);
     },
     //控制音乐播放/暂停的功能函数
-    async musicControl(isPlay, musicId) {
+    async musicControl(isPlay, musicId, musicLink) {
         // let backgroundAudioManager = wx.getBackgroundAudioManager();
         //我们应该更早一点创建这个实例
         if (isPlay) {
             //获取音乐的播放链接
-            let musicLinkData = await request('/song/url', { id: musicId });
-            let musicLink = musicLinkData.data[0].url;
+            if (!musicLink) {
+                let musicLinkData = await request('/song/url', { id: musicId });
+                musicLink = musicLinkData.data[0].url;
+                this.setData({
+                    musicLink
+                })
+            }
             //音乐播放
             //创建控制音乐播放的实例对象
             // let backgroundAudioManager = wx.getBackgroundAudioManager();
@@ -109,6 +164,29 @@ Page({
                 //注意：我是用let定义的变量，只能在if那个代码块中使用，else代码块它不认识
                 //所以我们应该把BackgroundAudioManager这个变量提到if的外面去
         }
+    },
+
+    //点击切歌的回调
+    handleSwitch(event) {
+        //获取切歌的类型
+        let type = event.currentTarget.id
+
+        //我要切歌的时候先关闭当前播放的音乐
+        this.backgroundAudioManager.stop();
+
+        // console.log(type);
+        //订阅来自recommendSong页面发布的musicId消息
+        PubSub.subscribe("musicId", (msg, musicId) => {
+                // console.log(msg, musicId);
+                //获取音乐详情信息
+                this.getMusicInfo(musicId);
+                //自动播放当前音乐
+                this.musicControl(true, musicId);
+                //取消订阅(订阅完拿到数据后就取消订阅，因为已经没有利用价值了)
+                PubSub.unsubscribe("musicId");
+            })
+            //发布消息数据给recommendSong页面
+        PubSub.publish('switchType', type)
     },
     /**
      * 生命周期函数--监听页面初次渲染完成
